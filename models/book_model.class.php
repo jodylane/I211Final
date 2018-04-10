f414287c8f72aa2c8ba19d065e8d7e68282d365b
Josh Lane <jodylane@umail.iu.edu>
2018-04-10 14:40:31 -0400

    did database stuff and edited comment on book model

--- models/book_model.class.php
+++ models/book_model.class.php
@@ -5,7 +5,7 @@
  * User: Josh Lane
  * Date: 4/5/2018
  * Time: 1:48 PM
- * Description: This file was created to send and retrieve calls from and to the database for the books table.
+ * Description: This file was created to send and retrieve calls from and to the database for the books.
  */
 class BookModel {
     private $db, $dbConnection, $tblBook, $tblBookCategory;
